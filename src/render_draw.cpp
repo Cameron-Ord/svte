b8453bdeabc383ca4278faa47b712ec7684d84b0
Cameron-Ord <cameron.ian.ronald.ord@outlook.com>
2025-03-18 18:58:17 -0600

    return correct values

--- src/render_draw.cpp
+++ src/render_draw.cpp
@@ -20,31 +20,34 @@ void FontRenderer::frender_set_renderer(SDL_Renderer *r) { rend = r; }
 
 //Returns the height in which the cursor reaches.
 int FontRenderer::render_curs(const Buf *buf, const Vec2i *dims) {
-    const int pad = 4;
+    const int pad = 2;
     int rowy = 0, colx = 0;
 
-    for (size_t i = 0; i <= buf->size; i++) {
-      const int x = (colx * dims->x) + pad;
-      const int y = (rowy * dims->y) + pad;
+    for (size_t i = 0; i <= buf->size && (int)i <= buf->pos; i++) {
+        const int x = (colx * dims->x) + pad;
+        const int y = (rowy * dims->y) + pad;
+ 
+        if (i == (size_t)buf->pos) {
+            SDL_Rect curs_rect = {x, y, dims->x, dims->y};
+            SDL_SetRenderDrawColor(rend, 255, 255, 255, 255);
+            SDL_RenderFillRect(rend, &curs_rect);
+            return rowy * dims->y + (pad * rowy);
+        }
 
-      const int is_nl = buf->buf[i] == NEWLINE;
-      switch(is_nl){
-          case 0:{
-              colx += 1;
-          }break;
-          case 1:{
-              colx = 0;
-              rowy += 1;
-          }break;
-      }
-
-      if (i == (size_t)buf->pos) {
-          SDL_Rect curs_rect = {x, y, dims->x, dims->y};
-          SDL_SetRenderDrawColor(rend, 255, 255, 255, 255);
-          SDL_RenderFillRect(rend, &curs_rect);
-      }
+        const int is_nl = buf->buf[i] == NEWLINE;
+        switch(is_nl){
+            default:
+                break;
+            case 0:{
+                colx += 1;
+            }break;
+            case 1:{
+                colx = 0;
+                rowy += 1;
+            }break;
+        }
     }   
-    return rowy * dims->y;
+    return 0;
 }
 //Returns the total height of the buffer when rendered.
 int FontRenderer::render_buffer(const Buf *buf, Chars *ch) {
@@ -58,7 +61,8 @@ int FontRenderer::render_buffer(const Buf *buf, Chars *ch) {
             
         const int is_nl = buf->buf[i] == NEWLINE;
         switch(is_nl){
-            default:break;
+            default:
+                break;
             case 0:{
                 Char_Tables *ct = ch->char_lookup(buf->buf[i]);
                 SDL_Rect char_rect = {x, y, ct->def.width, ct->def.height};
@@ -71,7 +75,7 @@ int FontRenderer::render_buffer(const Buf *buf, Chars *ch) {
             }break;
         }
     }
-    return rowy * m_ch_height;
+    return rowy * m_ch_height + (padding * rowy);
 }
 
 FontRenderer::~FontRenderer(void) {}
