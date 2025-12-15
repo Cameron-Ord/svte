ccb63403884bc4783dd3369319007e57f505c2b7
Cameron-Ord <cameron.ian.ronald.ord@outlook.com>
2025-12-15 14:43:18 -0700

    use makefile

--- bwin64.sh
+++
@@ -1,7 +0,0 @@
-x86_64-w64-mingw32-g++ \
-   src/*.cpp \
-  -o svte.exe \
-  -I/usr/x86_64-w64-mingw32/sys-root/mingw/include/SDL2 \
-  -L/usr/x86_64-w64-mingw32/sys-root/mingw/lib \
-  -lSDL2main -lSDL2 -lSDL2_ttf -mwindows -lm -g -Wall -Wpedantic
-
